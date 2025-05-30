7dd5af95f89e87eb153d3aa5d18581a96c602037
AliShariatian <shariatian92@gmail.com>
2025-05-30 20:21:58 +0330

    style: add inline code style

--- src/app/editor/_components/text-editor-section/output/_components/html-react-parser-options/index.tsx
+++ src/app/editor/_components/text-editor-section/output/_components/html-react-parser-options/index.tsx
@@ -1,24 +1,20 @@
+import { type HTMLElementType } from "react";
 import { type HTMLReactParserOptions, Element } from "html-react-parser";
 import { CodeBlock } from "./code-block";
 
-export const htmlReactParserOptions = () => {
-  const options: HTMLReactParserOptions = {
-    replace: (domNode) => {
-      if (domNode instanceof Element) {
-        switch (domNode.name) {
-          case "pre":
-            const language = domNode.attribs.language;
-            const showLineNumbers = domNode.attribs.linenumbers === "true";
-            const code = domNode.attribs.code;
+export const options: HTMLReactParserOptions = {
+  replace: (domNode) => {
+    if (domNode instanceof Element) {
+      switch (domNode.name as HTMLElementType) {
+        case "pre":
+          const code = domNode.attribs.code;
+          const language = domNode.attribs.language;
+          const showLineNumbers = domNode.attribs.linenumbers === "true";
+          return <CodeBlock code={code} language={language} showLineNumbers={showLineNumbers} />;
 
-            return <CodeBlock code={code} language={language} showLineNumbers={showLineNumbers} />;
-
-          default:
-            break;
-        }
+        default:
+          break;
       }
-    },
-  };
-
-  return { options };
+    }
+  },
 };
--- src/app/editor/_components/text-editor-section/output/index.tsx
+++ src/app/editor/_components/text-editor-section/output/index.tsx
@@ -3,15 +3,13 @@
 import { FC, ReactElement } from "react";
 
 import parse from "html-react-parser";
-import { htmlReactParserOptions } from "./_components/html-react-parser-options";
+import { options } from "./_components/html-react-parser-options";
 
 type Props = {
   content: string;
 };
 
 export const Output: FC<Props> = ({ content }): ReactElement => {
-  const { options } = htmlReactParserOptions();
-
   return (
     <div className="flex flex-col gap-4">
       <article className="bb p-4">{parse(content, options)}</article>
